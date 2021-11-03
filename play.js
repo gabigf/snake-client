59552264c0f28ea51a9aabf499105b5eb1d495b7
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-03 17:06:00 +0000

    Added function to connect to snek server

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = () => {
+	const conn = net.createConnection({
+		host: '165.227.47.243',
+		port: '50541'
+	});
+
+	conn.on('data', data => {
+		console.log(`We just received some data: ${data}`);
+	})
+
+	conn.setEncoding('utf-8');
+	return conn;
+}
+
+console.log('Connecting...');
+connect();
