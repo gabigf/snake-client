6e16187daf5abb5220d4470821616ba74d10a4cb
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-03 17:57:07 +0000

    Added exit command

--- play.js
+++ play.js
@@ -1,3 +1,22 @@
 const connect = require('./client');
 
+
+const setupInput = () => {
+	const stdin = process.stdin;
+	stdin.setRawMode(true);
+	stdin.setEncoding('utf8');
+	stdin.resume();
+	const handleUserInput = input => {
+		if (input === '\u0003') {
+			process.exit();
+		}
+	};
+
+	stdin.on('data', handleUserInput);
+
+	return stdin;
+}
+
+setupInput();
+
 connect();
